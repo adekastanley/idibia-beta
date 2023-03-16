6c707ee4894c9bc4e781e28695aa296847565b82
adeka stanley <adekastanley2@gmail.com>
2023-03-16 10:27:46 +0100

    preloader text almost done, will switch to image ater testing

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+paceOptions = {
+  ajax: true,
+  document:true
+
+  }
+
+Pace.on('done', ()=> {
+  
+  gsap.timeline()
+  .add('p')
+  .to('.pace',{
+    transform: 'scale(10,1)',
+    duration:4
+  },'+= 0.2')
+  .to('.pace', {
+    duration: 1,
+    height: '100%'
+  },"-=3")
+  .to('.loader-text', {
+    delay: .2,
+    duration: 3,
+    opacity: 0,
+    yPercent: 'BezierEasing(0.19,1,0.22,1)'
+  },'p')
+  .to('.title', {
+    duration: 2,
+    delay: .3,
+    y: -10,
+    opacity: 1,
+    ease: Expo.easeInOut
+  })
+})
