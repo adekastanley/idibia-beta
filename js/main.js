4ebf86d220b501758f9af598b5bd58fa0dd21a5f
adeka stanley <adekastanley2@gmail.com>
2023-03-19 01:45:46 +0100

    needs fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,129 @@
 //
 
 
+// navigation
+//
+const toggler = document.querySelector(".nav-btn");
+let rotated = false
+toggler.addEventListener('click', ()=> {
+  let lines = toggler.children
+  // console.log(lines[0])
+  if (rotated) {
+    lines[0].style.transform =' rotate(0deg)'
+    lines[2].style.transform =' rotate(0deg)'
+    lines[1].style.opacity =1
+
+
+
+    toggler.style.height =' 3rem'
+    rotated = false
+  } else {
+
+    lines[0].style.transform = ' rotate(45deg)'
+    lines[2].style.transform =' rotate(-45deg)'
+    lines[1].style.opacity =0
+    toggler.style.height =' 1.7rem'
+
+
+    rotated = true
+
+  }
+
+
+})
+
+
+gsap.timeline()
+.to(".nav-container", {
+  delay: 3,
+  duration: 1,
+  opacity: 1,
+  yPercent: "BezierEasing(0.19,1,0.22,1)",
+}, );
+
+let closed = true;
+toggler.addEventListener("click", () => {
+  if (closed) {
+    closed = false;
+    gsap.timeline().to(".ul-wrapper", {
+      x: 0,
+    });
+  } else {
+    closed = true;
+    gsap.timeline().to(".ul-wrapper", {
+      x: -250,
+    });
+  }
+})
+
+paceOptions = {
+  ajax: true,
+  document: true,
+};
+
+Pace.on("done", () => {
+  gsap
+    .timeline()
+    .add("p")
+    .to(
+      ".pace",
+      {
+        transform: "scale(10,1)",
+        duration: 4,
+      },
+      "+= 0.2"
+    )
+    .to(
+      ".pace",
+      {
+        duration: 1,
+        height: "100%",
+      },
+      "-=3"
+    )
+    .to(
+      ".loader-text",
+      {
+        delay: 0.2,
+        duration: 3,
+        opacity: 0,
+        yPercent: "BezierEasing(0.19,1,0.22,1)",
+      },
+      "p"
+    )
+    .to(
+      ".hero-img",
+      {
+        duration: 2,
+        delay: 0.3,
+        y: -10,
+        opacity: 1,
+        ease: Expo.easeInOut,
+      },
+      "-=2.5"
+    );
+});
+
+
+
+
+
+setTimeout(() => {
+  const containers = document.querySelectorAll('.container');
+  containers.forEach((target)=> {
+    target.style.display = 'flex'
+  })
+}, 3000);
+
+
+// 
+// section 2
+// 
+
+
+
 // particles 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', '../pluggins/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
@@ -127,127 +248,6 @@ particlesJS('particles-js',
   }
 
 );
-// navigation
-//
-const toggler = document.querySelector(".nav-btn");
-let rotated = false
-toggler.addEventListener('click', ()=> {
-  let lines = toggler.children
-  // console.log(lines[0])
-  if (rotated) {
-    lines[0].style.transform =' rotate(0deg)'
-    lines[2].style.transform =' rotate(0deg)'
-    lines[1].style.opacity =1
-
-
-
-    toggler.style.height =' 3rem'
-    rotated = false
-  } else {
-
-    lines[0].style.transform = ' rotate(45deg)'
-    lines[2].style.transform =' rotate(-45deg)'
-    lines[1].style.opacity =0
-    toggler.style.height =' 1.7rem'
-
-
-    rotated = true
-
-  }
-
-
-})
-
-
-gsap.timeline()
-.to(".nav-container", {
-  delay: 3,
-  duration: 1,
-  opacity: 1,
-  yPercent: "BezierEasing(0.19,1,0.22,1)",
-}, );
-
-let closed = true;
-toggler.addEventListener("click", () => {
-  if (closed) {
-    closed = false;
-    gsap.timeline().to(".ul-wrapper", {
-      x: 0,
-    });
-  } else {
-    closed = true;
-    gsap.timeline().to(".ul-wrapper", {
-      x: -250,
-    });
-  }
-})
-
-paceOptions = {
-  ajax: true,
-  document: true,
-};
-
-Pace.on("done", () => {
-  gsap
-    .timeline()
-    .add("p")
-    .to(
-      ".pace",
-      {
-        transform: "scale(10,1)",
-        duration: 4,
-      },
-      "+= 0.2"
-    )
-    .to(
-      ".pace",
-      {
-        duration: 1,
-        height: "100%",
-      },
-      "-=3"
-    )
-    .to(
-      ".loader-text",
-      {
-        delay: 0.2,
-        duration: 3,
-        opacity: 0,
-        yPercent: "BezierEasing(0.19,1,0.22,1)",
-      },
-      "p"
-    )
-    .to(
-      ".hero-img",
-      {
-        duration: 2,
-        delay: 0.3,
-        y: -10,
-        opacity: 1,
-        ease: Expo.easeInOut,
-      },
-      "-=2.5"
-    );
-});
-
-
-
-
-
-setTimeout(() => {
-  const containers = document.querySelectorAll('.container');
-  containers.forEach((target)=> {
-    target.style.display = 'flex'
-  })
-}, 3000);
-
-
-// 
-// section 2
-// 
-
-
-
 
 // text 
 
